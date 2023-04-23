f525e16c53511a856eac0a57795b1006bf11c388
mihawke <pratikgawari0000.com>
2023-04-23 18:44:15 +0530

    updated

---
+++ frontend/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+import { data } from './data/Data'
+function App() {
+  const [selectedOption, setSelectedOption] = useState(null)
+  const [selectedQuestion, setSelectedQuestion] = useState(0)
+  const [result, setResult] = useState(false)
+  function handleNext(event) {
+    event.preventDefault();
+    if (selectedOption !== null || selectedOption !== undefined) {
+      const isCorrect = data[selectedQuestion].options[selectedOption].isCorrect;
+      setResult(isCorrect);
+      setSelectedQuestion(selectedQuestion + 1)
+    }
+    setSelectedOption(null)
+  };
+
+  return (
+    <div className="app">
+      <div className='container'>
+        <p>{data[selectedQuestion].question}</p>
+        {data[selectedQuestion].options.map((option, index) => (
+          <div key={index} className='options'>
+            <input id={index} type='radio' name='option' value={index} onClick={(e) => setSelectedOption(e.target.value)} />
+            <label htmlFor={index}>{option.answer}</label>
+          </div>
+        ))}
+        <button onClick={handleNext}>Next</button>
+        {
+          result && <p>Correct</p>
+        }
+      </div>
+    </div>
+  );
+}
+
+export default App;
